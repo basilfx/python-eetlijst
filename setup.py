e8359183641a36eab2f9941650e7c5ed71b7be14
Bas Stottelaar <basstottelaar@gmail.com>
2014-03-29 19:17:51 +0100

    Add README and basic setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+# Setup definitions
+setup(
+    name="python-eetlijst",
+    version="1.0",
+    description="Basic Python API for Eetlijst.",
+    author="Bas Stottelaar",
+    author_email="basstottelaar@gmail.com",
+    py_modules=["eetlijst"],
+    install_requires=["requests", "beautifulsoup4"],
+    license = "GPLv3",
+    keywords = "python eetlijst api",
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: System :: Networking",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+    ],
+)
