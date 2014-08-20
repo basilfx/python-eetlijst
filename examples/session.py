e6f6adc0f6a1cf9c6418c526c22b4fd6a46972ee
Bas Stottelaar <basstottelaar@gmail.com>
2014-08-20 03:01:18 +0200

    Add example for continuing sessions.

---
+++ examples/session.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys, os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../"))
+
+import eetlijst
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        sys.stdout.write("Usage: %s <session_id>\n" % sys.argv[0])
+        return 0
+
+    # Parse session id
+    session_id = sys.argv[1].lower()
+
+    # Create a client
+    try:
+        client = eetlijst.Eetlijst(session_id=session_id, login=True)
+    except eetlijst.LoginError:
+        sys.stderr.write("Session id is probably expired or invalid.\n")
+        return 1
+
+    # Perform action
+    sys.stdout.write("Session id belongs to list with name '%s'.\n" % client.get_name())
+
+    return 0
+
+# E.g. `session.py session_id'
+if __name__ == "__main__":
+    sys.exit(main())
